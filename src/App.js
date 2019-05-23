a011a93ec81319b3ccb73d9c27e75fff8279477d
merhawi <merhawi.temnewo88@gmail.com>
2019-05-22 20:45:47 -0500

    Folder setup complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import Contact from "./pages/ContactPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/Default.js";
 
 class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return <h1>hello from tech store</h1>;
   }
 }
+
 export default App;
